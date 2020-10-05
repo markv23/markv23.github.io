3ad9ca2cddf420c437cdbf51d9f8f6e2c050b825
Mark Villafuerte <markv6900@gmail.com>
2020-10-04 23:43:49 -0500

    react_commit

---
+++ precache-manifest.9a93b52774cf5860da242e6e4d3ab756.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bb723dbfde2f347c9197c91a0b9ee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d05bfc919677c297f882",
+    "url": "/static/css/2.e08c6276.chunk.css"
+  },
+  {
+    "revision": "517ae3e9921a88fc7a80",
+    "url": "/static/css/main.fe1456e4.chunk.css"
+  },
+  {
+    "revision": "d05bfc919677c297f882",
+    "url": "/static/js/2.631943e9.chunk.js"
+  },
+  {
+    "revision": "6f9c91f92830cd94e0e9d23e81437a49",
+    "url": "/static/js/2.631943e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517ae3e9921a88fc7a80",
+    "url": "/static/js/main.07336d17.chunk.js"
+  },
+  {
+    "revision": "1cebdf7130c159a0297a",
+    "url": "/static/js/runtime-main.c40c2c89.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
